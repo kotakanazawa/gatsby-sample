1f51021231bc136906fe86fa6fe5e1b57b6eed8c
Kota Kanazawa <kota0818@gmail.com>
2020-09-30 10:56:23 +0900

    Add Layout

---
+++ src/components/layout.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "./header"
+import Footer from "./footer"
+import "./layout.css"
+
+export default ({ children }) => (
+    <div>
+      <Header />
+      { children }
+      <Footer />
+    </div>
+  )
