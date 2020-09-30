26d286c58a045967d510909c75b987268beff6ae
Kota Kanazawa <kota0818@gmail.com>
2020-09-30 11:32:04 +0900

    Fix fontawesome icon

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,10 @@ import Header from "./header"
 import Footer from "./footer"
 import "./layout.css"
 
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"
+config.autoAddCss = false
+
 export default ({ children }) => (
     <div>
       <Header />
