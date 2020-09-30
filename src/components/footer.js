0b11f3411d396d1f9c3a164c8995d787a292cf0a
Kota Kanazawa <kota0818@gmail.com>
2020-09-30 11:45:54 +0900

    Add SNS icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,17 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faTwitter,
+  faFacebookSquare,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons"
 
 export default () => {
   const data = useStaticQuery(graphql`
     query {
-      pattern: file(relativePath: {eq: "pattern.jpg"}) {
+      pattern: file(relativePath: { eq: "pattern.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 1920, quality: 100) {
             ...GatsbyImageSharpFluid_withWebp
@@ -38,19 +44,19 @@ export default () => {
         <ul className="sns">
           <li>
             <a href="https://twitter.com/">
-              <i className="fab fa-twitter" />
+              <FontAwesomeIcon icon={faTwitter} />
               <span className="sr-only">Twitter</span>
             </a>
           </li>
           <li>
             <a href="https://facebook.com/">
-              <i className="fab fa-facebook-square" />
+              <FontAwesomeIcon icon={faFacebookSquare} />
               <span className="sr-only">Facebook</span>
             </a>
           </li>
           <li>
             <a href="http://instagram.com/">
-              <i className="fab fa-instagram" />
+              <FontAwesomeIcon icon={faInstagram} />
               <span className="sr-only">Instagram</span>
             </a>
           </li>
