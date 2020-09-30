f4db7767c9480d2aadcdaaa29aa1e9f6e36fccca
Kota Kanazawa <kota0818@gmail.com>
2020-09-30 15:38:27 +0900

    Set PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,19 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-plugin-react-helmet`
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `ESSENTIALS エッセンシャルズ`,
+        short_name: `ESSENTIALS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#477294`,
+        display: `standalone`,
+        icon: `src/images/icon.png`
+      }
+    },
+    `gatsby-plugin-offline`,
   ],
 }
